ee4022f7f26ff41a4bc474f283a4261180f0ef99
erictiong <64233298+erictiong@users.noreply.github.com>
2020-05-02 13:31:55 -0500

    first commit

--- src/main/java/com/example/demospringcloudconfigclient/DemoSpringCloudconfigClientApplication.java
+++ src/main/java/com/example/demospringcloudconfigclient/DemoSpringCloudconfigClientApplication.java
@@ -2,6 +2,7 @@ package com.example.demospringcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoSpringCloudconfigClientApplication {
@@ -9,5 +10,4 @@ public class DemoSpringCloudconfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringCloudconfigClientApplication.class, args);
     }
-
 }
